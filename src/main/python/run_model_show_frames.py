de31d5888166bbcafdb958a4bf32f5906c049d5f
Nikolaj Goodger <ngoodger@gmail.com>
2018-10-06 21:00:11 +1000

    Remove writing frames twice during test.

--- src/main/python/run_model_show_frames.py
+++ src/main/python/run_model_show_frames.py
@@ -17,8 +17,8 @@ def run_model_show_frames():
         batch_data = {"force": data[0], "s": data[1], "seq_len": SEQ_LEN}
         s = data[1]
         y1_list = []
+        s_initial = batch_data["s"][0]
         for i in range(SEQ_LEN - 1):
-            s_initial = batch_data["s"][0]
             force_0 = batch_data["force"][i]
             force_1 = batch_data["force"][i + 1]
             if i == 0:
@@ -30,13 +30,6 @@ def run_model_show_frames():
                     None, recurrent_state, force_0, force_1, first_iteration=False
                 )
             y1_list.append(y1)
-        seq_idx = 0
-        for i in range(4):
-            s0_frame = s[seq_idx][0, :, :, :, i].data.numpy()
-            bs.render(
-                s0_frame.reshape([bs.GRID_SIZE, bs.GRID_SIZE]),
-                "_{}_{}".format(str(seq_idx), str(i)),
-            )
         for seq_idx in range(SEQ_LEN - 1):
             for i in range(4):
                 s0_frame = s[seq_idx + 1][0, :, :, :, i].data.numpy()
