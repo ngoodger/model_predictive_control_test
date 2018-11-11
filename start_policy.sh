257cbe7f01fd92901de92a998f0ca397f123fea3
Nikolaj Goodger <ngoodger@gmail.com>
2018-11-12 08:53:07 +1100

    Added runner scripts for training model and policy.

--- start_policy.sh
+++ start_policy.sh
@@ -3,7 +3,7 @@ export WORLD_SIZE=1
 export MASTER_PORT=8080
 export MASTER_ADDR=127.0.0.1
 export RANK=0
-python3 src/main/python/train_model.py &
+python3 src/main/python/train_policy.py
 #export RANK=1
 #python3 src/main/python/train_model.py &
 #export RANK=2
