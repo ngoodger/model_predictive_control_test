857673f33d28961b943a6b44e28fab462eeab2d7
Nikolaj Goodger <ngoodger@gmail.com>
2018-11-15 22:42:32 +1100

    Updated cluster name.

--- setup_gke.sh
+++ setup_gke.sh
@@ -1,4 +1,4 @@
-CLUSTER_NAME=standard-cluster-2
+CLUSTER_NAME=standard-cluster-3
 gcloud container clusters get-credentials $CLUSTER_NAME --zone us-central1-a
 kubectl create clusterrolebinding default-admin \
       --clusterrole=cluster-admin --user=$(gcloud config get-value account)
