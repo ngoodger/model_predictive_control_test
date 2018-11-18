25323fc099098c44af2e31f3b6cba71fef7fc67a
Nikolaj Goodger <ngoodger@gmail.com>
2018-11-18 20:41:56 +1100

    Get cluster name from file.

--- setup_gke.sh
+++ setup_gke.sh
@@ -1,4 +1,4 @@
-CLUSTER_NAME=standard-cluster-3
+CLUSTER_NAME=$(cat GKE_CLUSTER_NAME)
 gcloud container clusters get-credentials $CLUSTER_NAME --zone us-central1-a
 kubectl create clusterrolebinding default-admin \
       --clusterrole=cluster-admin --user=$(gcloud config get-value account)
