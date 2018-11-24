47b9701bc8d941bb0d19bc64ae5ad7e807ce4f62
Nikolaj Goodger <ngoodger@gmail.com>
2018-11-24 23:55:26 +1100

    Added GCS variable to start scripts.

--- start_policy.sh
+++ start_policy.sh
@@ -3,6 +3,7 @@ export WORLD_SIZE=1
 export MASTER_PORT=8080
 export MASTER_ADDR=127.0.0.1
 export RANK=0
+export GCS_BUCKET=mpc-test
 python3 src/main/python/train_policy.py
 #export RANK=1
 #python3 src/main/python/train_model.py &
