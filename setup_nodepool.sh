6c36553b076fdb46a75ecd753f7d1965a9bff653
Nikolaj Goodger <ngoodger@gmail.com>
2018-11-25 10:32:51 +1100

    Changed gpu nodepool to use small ssd for faster image pull.

--- setup_nodepool.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CLUSTER_NAME=$(cat GKE_CLUSTER_NAME)
-gcloud container node-pools create cpu-pool --cluster=$CLUSTER_NAME --machine-type=n1-highcpu-2 --zone=us-central1-a --num-nodes=1 --preemptible
-gcloud container node-pools create gpu-pool --cluster=$CLUSTER_NAME --machine-type=n1-standard-2 --accelerator=type=nvidia-tesla-p4,count=1 --enable-autoscaling --max-nodes=1 --min-nodes=0 --preemptible --zone=us-central1-a --num-nodes=0
