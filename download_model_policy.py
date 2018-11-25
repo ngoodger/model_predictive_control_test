13be3edda85b0c75d1a30a17b28ba4aa37b5efa2
Nikolaj Goodger <ngoodger@gmail.com>
2018-11-25 22:18:03 +1100

    Fixed download policy.

--- download_model_policy.py
+++ download_model_policy.py
@@ -1,8 +1,11 @@
 from google.cloud import storage
-MODEL_PATH = "my_model.pt"
+INPUT_CNN_PATH = "input_cnn.pt"
+MODEL_PATH = "recurrent_model.pt"
 POLICY_PATH = "my_policy.pt"
 client = storage.Client()
 bucket = client.get_bucket("mpc-test")
+blob = bucket.blob(INPUT_CNN_PATH)
+blob.download_to_filename(INPUT_CNN_PATH)
 blob = bucket.blob(MODEL_PATH)
 blob.download_to_filename(MODEL_PATH)
 blob = bucket.blob(POLICY_PATH)
