4492a6a3f5509df9cb06a7f2a93e02b2c391cf4e
Nikolaj Goodger <ngoodger@gmail.com>
2018-12-02 18:46:55 +1100

    Updated training to use 4 gpus.

--- setup_nodepool_gpu.sh
+++ setup_nodepool_gpu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CLUSTER_NAME=$(cat GKE_CLUSTER_NAME)
 gcloud container node-pools create cpu-pool --cluster=$CLUSTER_NAME --machine-type=n1-standard-2 --zone=us-central1-a --num-nodes=1 --preemptible
-gcloud container node-pools create gpu-pool --cluster=$CLUSTER_NAME --machine-type=n1-standard-4 --accelerator=type=nvidia-tesla-p4,count=1 --enable-autoscaling --max-nodes=1 --min-nodes=0 --preemptible --zone=us-central1-a --num-nodes=0 --disk-size=30 --disk-type=pd-ssd
+gcloud container node-pools create gpu-pool --cluster=$CLUSTER_NAME --machine-type=n1-standard-4 --accelerator=type=nvidia-tesla-p4,count=0 --enable-autoscaling --max-nodes=4 --min-nodes=0 --preemptible --zone=us-central1-a --num-nodes=0 --disk-size=30 --disk-type=pd-ssd
